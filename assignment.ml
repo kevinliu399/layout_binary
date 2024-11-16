9d945eeb611ece20f8bd1b298806a1068b2be284
Kevin Liu <kevin.liu4@mail.mcgill.ca>
2024-11-16 18:56:42 -0500

    added steps

--- assignment.ml
+++ assignment.ml
@@ -56,16 +56,20 @@ let get_xf tree = match tree with
 ************************** MAIN ALGORITHM ***************************
 ********************************************************************)
 
+(* Step 1: Assign initial x and mod values *)
 let rec traversal_one (tree: 'a tree) (is_right: bool) (prev: 'a tree option) (depth: int) : unit =
   raise NotImplemented
 
+(* Step 2: Fix overlap and assign shift value *)
 let rec traversal_two (tree: 'a tree) (ancestor_mods: float list) (right_sibling: 'a tree option) : contour = 
   (* Refer to the top of the code for helper functions that can assist you in the implementation *)
   raise NotImplemented
 
+(* Step 3: Compute final x positions for all nodes *)
 let traversal_three (tree: 'a tree) : unit =
   raise NotImplemented
 
+(* Step 4: Fix the root node *)
 let fix_root (tree: 'a tree) =
   raise NotImplemented
 
